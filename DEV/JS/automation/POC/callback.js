090428cc01281e0271f6fe5d74437ca4ebc97e20
adityasharma2020 <adityasharma.1.tech@gmail.com>
2022-04-26 22:02:11 +0530

    Callback vs promises

--- DEV/JS/automation/POC/callback.js
+++ DEV/JS/automation/POC/callback.js
@@ -1,6 +1,34 @@
 // // read 2 files f1 and f2
 
-const fs =require("fs");
+const fs = require('fs');
+
+//callback hell -->pyramind of doom
+fs.readFile('f1.txt', function cb1(err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res + '');
+    fs.readFile('f2.txt', function cb2(err, res) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(res + '');
+        fs.readFile('f3.txt', function (err, res) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(res + '');
+            console.log('read all files.');
+          }
+        });
+      }
+    });
+  }
+});
+
+
+//one way to overcome callback hell is to write sepeate callback functions.
+
 
 // fs.readFile("f1.txt",cb);
 
@@ -24,17 +52,16 @@ const fs =require("fs");
 //     }
 // }
 
-
 //read 3 files f1.txt f2.txt f3.txt parallely using callbacks
 
-fs.readFile("f1.txt",cb);
-fs.readFile("f2.txt",cb);
-fs.readFile("f3.txt",cb);
+fs.readFile('f1.txt', cb);
+fs.readFile('f2.txt', cb);
+fs.readFile('f3.txt', cb);
 
-function cb(err,res){
-    if(err){
-        console.log(err);
-    }else{
-        console.log(res+"");
-    }
+function cb(err, res) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(res + '');
+  }
 }
--- DEV/JS/automation/POC/promises_interview.js
+++ DEV/JS/automation/POC/promises_interview.js
@@ -26,3 +26,18 @@ f1readPromise
   .catch(function (err) {
     console.log(err);
   });
+
+  //q2. read all three files parallely
+
+
+  let f1p = fs.promises.readFile('f1.txt');
+  let f2p = fs.promises.readFile('f2.txt');
+  let f3p = fs.promises.readFile('f3.txt');
+
+  f1p.then(cb);
+  f2p.then(cb);
+  f3p.then(cb);
+
+  function cb(req,res){
+    console.log(data + "");
+  }
