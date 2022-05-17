07b19ee92a3dbbabe99a1647ccc9757f81ef257b
adityasharma2020 <adityasharma.1.tech@gmail.com>
2022-05-17 22:00:19 +0530

    mongodb started

--- CWH/DacneWebsite/app.js
+++ CWH/DacneWebsite/app.js
@@ -12,6 +12,9 @@ app.set('view engine', 'pug');
 app.get('/', (req, res) => {
   res.status(200).render('index.pug');
 });
+app.get("/contact", (req, res)=>{ 
+  res.status(200).send('contact.pug');
+});
 
 
 app.listen(port,()=>{
