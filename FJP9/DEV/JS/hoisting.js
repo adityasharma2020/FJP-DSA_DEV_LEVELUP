46cb8e354ee023ca004cb1fbabd8c122697fb62e
adityasharma2020 <adityasharma.1.tech@gmail.com>
2022-06-26 23:33:52 +0530

    hoisting revision

--- FJP9/DEV/JS/hoisting.js
+++ FJP9/DEV/JS/hoisting.js
@@ -1,4 +1,20 @@
-console.log(a);
+// // console.log(a);
 
-var a=10;
-console.log(a);
+// var a = 10;
+
+// function fun() {
+//   console.log('helllo world!');
+// }
+// // console.log(fun());
+// fun()
+
+function a(){
+  c();
+  function c(){
+    console.log(b);
+  }
+}
+
+a();
+var b = 10;
+console.log(b);
