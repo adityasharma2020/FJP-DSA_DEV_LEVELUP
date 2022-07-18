0b45f14fb0555ade72a64f54d8d42118c930785b
adityasharma2020 <adityasharma.1.tech@gmail.com>
2022-07-18 23:44:37 +0530

    this continued

--- FJP9/DEV/JS/this.js
+++ FJP9/DEV/JS/this.js
@@ -1,6 +1,49 @@
-function type1(){
-  console.log(this.nam);
+// <====================RULES =============>
+
+// 1. the value of this is evaluated during the run time.
+// 2.value of this is evaluated during the run time.
+// 3.this points to that object from where the fucntion is called.
+
+
+//1
+// function type1(){
+//   console.log(this.name);
+// }
+
+// var name = "javascript";
+// type1();//when function is called globally , this always points to window object.
+
+// //2
+// function type2(){
+//   console.log(this.lname);
+// }
+// var lname = "tiwari";
+// var obj = {
+  
+// }
+
+var food ="pziza";
+var obj = {
+  food :"psata",
+  eat(){
+    console.log(this.food);
+  }
 }
 
-var nam = "javascript";
-type1();
+var foo = obj.eat;
+foo();
+
+
+var length =1;
+function square(){
+  let cb = function (){
+    console.log(this.length *this.length);
+  }
+  setTimeout(cb,2000);
+}
+
+var object ={
+  length :3,
+  square
+}
+obj.square();
