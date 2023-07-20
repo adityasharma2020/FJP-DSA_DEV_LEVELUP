a724f8c3087ed1f89f88fb777f1198b259e65eb7
adityasharma2020 <aditya15116617@gmail.com>
2023-07-21 02:52:33 +0530

    a simple blockchain implemented using typescript

--- NC-online_projects/basicBlockChainWithTypescript/src/index.ts
+++ NC-online_projects/basicBlockChainWithTypescript/src/index.ts
@@ -1,4 +1,4 @@
-import * as crypto from "crypto";
+import crypto from "crypto";
 
 interface BlockShape {
   hash: string;
@@ -7,6 +7,7 @@ interface BlockShape {
   data: string;
 }
 
+//here we are creating a block
 class Block implements BlockShape {
   public hash: string;
   constructor(
@@ -14,10 +15,50 @@ class Block implements BlockShape {
     public height: number,
     public data: string
   ) {
+    // so , when we get the data we hash the data
     this.hash = Block.calculateHash(prevHash, height, data);
   }
 
-  static calculateHash(prevHash:string,height:number,data:string){
-    const toHash = `${prevHash} ${}`
+  // this function use  the library "cryto" to convert  our data into hash.
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash} ${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class BlockChain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+
+  public getBlocks(){
+    return [...this.blocks];
+  }
+}
+
+
+
+
+const blockchain = new BlockChain();
+blockchain.addBlock("first one");
+blockchain.addBlock("second one");
+blockchain.addBlock("third one");
+blockchain.addBlock("four one");
+blockchain.addBlock("fifth one");
+blockchain.getBlocks().push(new Block("xxx",111,"hackedddd"));
+
+console.log(blockchain.getBlocks());
