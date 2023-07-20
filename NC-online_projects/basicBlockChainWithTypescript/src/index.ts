c960104345cddf59a05f44e059da41562d4d5058
adityasharma2020 <aditya15116617@gmail.com>
2023-07-21 02:17:01 +0530

    blockchain classes setting up

---
+++ NC-online_projects/basicBlockChainWithTypescript/src/index.ts
@@ -0,0 +1,23 @@
+import * as crypto from "crypto";
+
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+
+  static calculateHash(prevHash:string,height:number,data:string){
+    const toHash = `${prevHash} ${}`
+  }
+}
---
+++ NC-online_projects/basicBlockChainWithTypescript/src/myPackage.js
@@ -0,0 +1,22 @@
+// @ts-check
+/**
+ * Initializes the project
+ * @param {object} config 
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * 
+ * @returns {boolean}
+ */
+export function init(config){
+    return true;
+}
+
+
+/**
+ * 
+ * @param {number} code 
+ * @returns {number}
+ */
+export function exit (code){
+    return code +1;
+}
