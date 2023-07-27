9e413c418e15e268731a70c65add3356e61096f7
adityasharma2020 <aditya15116617@gmail.com>
2023-07-28 00:45:02 +0530

    game completed

--- DSA Based Projects/snake mania game/mycode/js/index.js
+++ DSA Based Projects/snake mania game/mycode/js/index.js
@@ -1,59 +1,122 @@
-// game constants and variables
-let direction = { x: 0, y: 0 }
+// Game Constants & Variables
+let inputDir = { x: 0, y: 0 }
 const foodSound = new Audio('music/food.mp3')
 const gameOverSound = new Audio('music/gameover.mp3')
 const moveSound = new Audio('music/move.mp3')
 const musicSound = new Audio('music/music.mp3')
-let speed = 2
+let speed = 9
+let score = 0
 let lastPaintTime = 0
-let snakeArr = [{ x: 5, y: 10 }]
-let board = document.getElementById('board')
-let food = { x: 6, y: 7 }
+let snakeArr = [{ x: 7, y: 13 }]
 
-//game functions
-function main(currentTime) {
+food = { x: 6, y: 7 }
+
+// Game Functions
+function main(ctime) {
   window.requestAnimationFrame(main)
-
-  if (currentTime - lastPaintTime / 1000 < 1 / speed) {
+  // console.log(ctime)
+  if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
     return
   }
-
-  lastPaintTime = currentTime
+  lastPaintTime = ctime
   gameEngine()
 }
 
-function gameEngine() {
-  // part1 : updating the snake arrray
+function isCollide(snake) {
+  // If you bump into yourself
+  for (let i = 1; i < snakeArr.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
+      return true
+    }
+  }
+  // If you bump into the wall
+  if (snake[0].x >=15 || snake[0].x <= 0 || snake[0].y >= 15 || snake[0].y <= 0) {
+    return true
+  }
 
-  //part 2: render the snake and food
+  return false
+}
+
+function gameEngine() {
+  // Part 1: Updating the snake array & Food
+  if (isCollide(snakeArr)) {
+    gameOverSound.play()
+    musicSound.pause()
+    inputDir = { x: 0, y: 0 }
+    alert('Game Over. Press any key to play again!')
+    snakeArr = [{ x: 7, y: 13 }]
+    musicSound.play()
+    score = 0
+  }
+
+  // If you have eaten the food, increment the score and regenerate the food
+  if (snakeArr[0].y === food.y && snakeArr[0].x === food.x) {
+    foodSound.play()
+    score += 1
+    if (score > hiscoreval) {
+      hiscoreval = score
+      localStorage.setItem('hiscore', JSON.stringify(hiscoreval))
+      hiscoreBox.innerHTML = 'HiScore: ' + hiscoreval
+    }
+    scoreBox.innerHTML = 'Score: ' + score
+    snakeArr.unshift({
+      x: snakeArr[0].x + inputDir.x,
+      y: snakeArr[0].y + inputDir.y,
+    })
+    let a = 2
+    let b = 14
+    food = {
+      x: Math.round(a + (b - a) * Math.random()),
+      y: Math.round(a + (b - a) * Math.random()),
+    }
+  }
+
+  // Moving the snake
+  for (let i = snakeArr.length - 2; i >= 0; i--) {
+    snakeArr[i + 1] = { ...snakeArr[i] }
+  }
+
+  snakeArr[0].x += inputDir.x
+  snakeArr[0].y += inputDir.y
+
+  // Part 2: Display the snake and Food
+  // Display the snake
   board.innerHTML = ''
-  //   display snake
   snakeArr.forEach((e, index) => {
     snakeElement = document.createElement('div')
     snakeElement.style.gridRowStart = e.y
     snakeElement.style.gridColumnStart = e.x
+
     if (index === 0) {
       snakeElement.classList.add('head')
     } else {
-      snakeElement.classList.add(`snake`)
+      snakeElement.classList.add('snake')
     }
     board.appendChild(snakeElement)
   })
-
-  //   display food
+  // Display the food
   foodElement = document.createElement('div')
   foodElement.style.gridRowStart = food.y
   foodElement.style.gridColumnStart = food.x
-  foodElement.classList.add(`food`)
+  foodElement.classList.add('food')
   board.appendChild(foodElement)
 }
 
-// to render our screen
+// Main logic starts here
+
+let hiscore = localStorage.getItem('hiscore')
+if (hiscore === null) {
+  hiscoreval = 0
+  localStorage.setItem('hiscore', JSON.stringify(hiscoreval))
+} else {
+  hiscoreval = JSON.parse(hiscore)
+  hiscoreBox.innerHTML = 'HiScore: ' + hiscore
+}
+
 window.requestAnimationFrame(main)
 window.addEventListener('keydown', (e) => {
-  inputDir = { x: 0, y: 1 } // Start the game
+  inputDir = { x: 0, y: -1 } // Start the game
   moveSound.play()
-
   switch (e.key) {
     case 'ArrowUp':
       console.log('ArrowUp')
