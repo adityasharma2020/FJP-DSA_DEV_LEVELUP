c04d2234d6ccfbaaa93e9c49b27afb521b8978f1
adityasharma2020 <aditya15116617@gmail.com>
2023-08-09 10:46:35 +0530

    nodemailer Integrated

--- BACKEND/FileUpload/models/File.js
+++ BACKEND/FileUpload/models/File.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-
+const nodemailer = require('nodemailer')
+require('dotenv').config()
 const fileSchema = mongoose.Schema({
   name: {
     type: String,
@@ -19,4 +20,30 @@ const fileSchema = mongoose.Schema({
   },
 })
 
+//postMiddleware
+fileSchema.post('save', async function (doc) {
+  try {
+    console.log('Doc', doc)
+    //transporter
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    })
+
+    //send mail
+    let info = await transporter.sendMail({
+      from: 'Aditya15116617@gmail.com ',
+      to: doc.email,
+      subject: 'new file uploaded to cloudinary',
+      html: `<h2>hello file uploaded</h2> view here: <p>link: <a href =${doc.imageUrl}>click here to see</p>`,
+    })
+
+    console.log(info)
+  } catch (error) {
+    console.log(error)
+  }
+})
 module.exports = mongoose.model('File', fileSchema)
