aabb337903bfbb9eadc5c72383ac6bcc1ca6f3c6
adityasharma2020 <aditya15116617@gmail.com>
2023-08-11 04:19:05 +0530

    mongodb cluster setup

--- WHATSAPP/whatsapp_backend/src/server.js
+++ WHATSAPP/whatsapp_backend/src/server.js
@@ -1,16 +1,40 @@
+import mongoose from 'mongoose'
 import app from './app.js'
 import logger from './config/logger.config.js'
 
 //env variables
 const PORT = process.env.PORT || 5000
+const DATABASE_URL = process.env.DATABASE_URL
 
+// -----------------mongodb setup-------------------------------------
+//exit on mongodb error
+mongoose.connection.on('error', (error) => {
+  logger.error(`MONGOdb Connection error: ${error}`)
+  process.exit(1)
+})
+
+//mongodb debug mode
+if (process.env.NODE_ENV != 'production') {
+  mongoose.set('debug', true)
+}
+
+//mongodb connection
+mongoose
+  .connect(DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    logger.info('connected to MONGODB.')
+  })
+// -----------------------------------------------------------------------
 let server = app.listen(PORT, () => {
   logger.info(`server is listening at ${PORT}`)
-  console.log('process id', process.pid)
+
   // throw new Error('error in server')
 })
 
-//----------handle server errors----------------------
+//----------handle server errors--------------
 const exitHandler = () => {
   if (server) {
     logger.info('server closed')
@@ -28,7 +52,7 @@ const unexpectedErrorHandler = (error) => {
 process.on('uncaughtException', unexpectedErrorHandler)
 process.on('unhandledRejection', unexpectedErrorHandler)
 
-//SIGTERMSIGTERM is a signal in Unix-like operating systems that stands for 
+//SIGTERMSIGTERM is a signal in Unix-like operating systems that stands for
 //"termination signal." It's a way for a process to receive a request to terminate gracefully.
 process.on('SIGTERM', () => {
   if (server) {
