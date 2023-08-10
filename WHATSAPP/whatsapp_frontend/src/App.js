6140c87c593883546898e076c52e302e737cace8
adityasharma2020 <aditya15116617@gmail.com>
2023-08-11 04:42:35 +0530

    frontend Setup

---
+++ WHATSAPP/whatsapp_frontend/src/App.js
@@ -0,0 +1,12 @@
+import {ChatIcon} from "./svg"
+
+function App() {
+  return (
+    <div>
+      <h1>hello form the app.js frontend</h1>
+      <ChatIcon className="red"></ChatIcon>
+    </div>
+  );
+}
+
+export default App;
