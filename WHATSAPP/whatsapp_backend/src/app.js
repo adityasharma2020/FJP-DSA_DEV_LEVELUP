55d7071ba96b8937f0ee205cccd03cca00bcf5ba
adityasharma2020 <aditya15116617@gmail.com>
2023-08-11 02:43:28 +0530

    required middlewares added

---
+++ WHATSAPP/whatsapp_backend/src/app.js
@@ -0,0 +1,54 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import mongoSanitize from 'express-mongo-sanitize'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import fileUpload from 'express-fileupload'
+import cors from 'cors'
+
+// dotenv configuration
+dotenv.config()
+
+//create express app
+const app = express()
+
+//morgan
+if (process.env.NODE_ENV != 'production') {
+  app.use(morgan('dev'))
+}
+//helmet
+app.use(helmet())
+
+//parse josn request url
+app.use(express.urlencoded({ extended: true }))
+
+//parse json request body
+app.use(express.json())
+
+//express mongo sanitize user data
+app.use(mongoSanitize())
+
+// cookie parser
+app.use(cookieParser())
+
+//gzip compression
+app.use(compression())
+
+//express fileupload
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: '/tmp/',
+  })
+)
+
+//cors
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('hello from the server')
+})
+
+export default app
--- WHATSAPP/whatsapp_backend/src/server.js
+++ WHATSAPP/whatsapp_backend/src/server.js
@@ -1,1 +1,9 @@
-console.log("welocme nodejs app");
+import app from './app.js'
+
+
+//env variables
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`server is listening at ${PORT}`)
+})
