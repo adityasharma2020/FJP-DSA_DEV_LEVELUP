b438931f458b9854bb4ae7d0e21bf95d676abf40
adityasharma2020 <aditya15116617@gmail.com>
2023-08-15 08:57:07 +0530

    react form, react router ,GIF generator project starter

--- FRONTEND/react-form/src/index.js
+++ FRONTEND/react-form/src/index.js
@@ -1,12 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-import { BrowserRouter } from 'React-Router-Dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
     <App />
-  </BrowserRouter>
-)
+);
+
