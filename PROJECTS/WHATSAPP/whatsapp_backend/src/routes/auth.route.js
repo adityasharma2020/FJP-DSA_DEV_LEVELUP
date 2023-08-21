f472e12191d243dee46d625d656198f57f8bdbb1
adityasharma2020 <aditya15116617@gmail.com>
2023-08-21 23:29:17 +0530

    authorization done with middleware

--- PROJECTS/WHATSAPP/whatsapp_backend/src/routes/auth.route.js
+++ PROJECTS/WHATSAPP/whatsapp_backend/src/routes/auth.route.js
@@ -6,12 +6,17 @@ import {
   refreshToken,
   register,
 } from '../controllers/auth.controller.js'
+import authMiddleware from '../middlewares/authMiddleware.js'
+
 const router = express.Router()
 router.post('/login')
 
-router.route('/register').post(trimRequest.all,register)
-router.route('/login').post(trimRequest.all,login)
-router.route('/logout').post(trimRequest.all,logout)
-router.route('/refreshtoken').post(trimRequest.all,refreshToken)
+router.route('/register').post(trimRequest.all, register)
+router.route('/login').post(trimRequest.all, login)
+router.route('/logout').post(trimRequest.all, logout)
+router.route('/refreshtoken').post(trimRequest.all, refreshToken)
+router
+  .route('/testroute')
+  .get(trimRequest.all, authMiddleware, (req, res) => res.send('hello'))
 
 export default router
