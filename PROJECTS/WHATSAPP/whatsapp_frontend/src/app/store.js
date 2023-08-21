e6fe09e2228419fbef39ecf4c9cb69bd63895dc0
adityasharma2020 <aditya15116617@gmail.com>
2023-08-21 14:48:22 +0530

    redux store setup

--- PROJECTS/WHATSAPP/whatsapp_frontend/src/app/store.js
+++ PROJECTS/WHATSAPP/whatsapp_frontend/src/app/store.js
@@ -1,9 +1,7 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userSlice from '../features/userSlice'
 
-const rootReducer = combineReducers({
-    user:userSlice
-})
+const rootReducer = combineReducers({})
 
 export const store = configureStore({
   reducer: rootReducer,
